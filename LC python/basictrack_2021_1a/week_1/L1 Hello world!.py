a3195f91156406515136459c835b6edfdcbcf374
Cheng Hao Tsai <chenghaotsai@RN-145-97-141-170.eduroam.rug.nl>
2020-09-22 16:49:32 +0200

    update 0922

---
+++ LC python/basictrack_2021_1a/week_1/L1 Hello world!.py
@@ -0,0 +1,1 @@
+print("please write each file in different file")
---
+++ LC python/basictrack_2021_1a/week_2/variables.py
@@ -0,0 +1,12 @@
+#Get information
+ask_user = float(input("Please give me a number"))
+    #int makes the answer(string) a number
+
+#Do calculation
+calculated_answer = ask_user + 2
+
+#Give output
+print("answer= ", calculated_answer)
+
+
+#see 38min about commit the changes at Github#
